f28e8cc04a3774a88c0d850d83348765c7511659
sravan <mekalasravankumar66@gmail.com>
2026-05-01 08:23:25 +0530

    fix env

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,20 +1,46 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-});
+const BASE_URL = import.meta.env.VITE_API_URL || "https://homeiq-5av2.onrender.com";
 
-export const registerUser = (data) => API.post('/auth/register', data)
-export const loginUser = (data) => API.post('/auth/login', data)
-export const getRooms = () => API.get('/rooms/')
-export const createRoom = (data) => API.post('/rooms/', data)
-export const deleteRoom = (id) => API.delete(`/rooms/${id}`)
-export const getDevices = () => API.get('/devices/')
-export const getDevicesByRoom = (roomId) => API.get(`/devices/room/${roomId}`)
-export const createDevice = (data) => API.post('/devices/', data)
-export const controlDevice = (id, data) => API.patch(`/devices/${id}/control`, data)
-export const deleteDevice = (id) => API.delete(`/devices/${id}`)
-export const getLogs = () => API.get('/logs/')
-export const createSchedule = (data) => API.post('/schedules/', data)
-export const getSchedules = () => API.get('/schedules/')
-export const createGuest = (data) => API.post('/guests/', data)
+export const registerUser = async (userData) => {
+    return await axios.post(`${BASE_URL}/auth/register`, userData);
+}
+export const loginUser = async (data) => {
+    return await axios.post(`${BASE_URL}/auth/login`, data);
+}
+export const getRooms = async () => {
+    return await axios.get(`${BASE_URL}/rooms/`);
+}
+export const createRoom = async (data) => {
+    return await axios.post(`${BASE_URL}/rooms/`, data);
+}
+export const deleteRoom = async (id) => {
+    return await axios.delete(`${BASE_URL}/rooms/${id}`);
+}
+export const getDevices = async () => {
+    return await axios.get(`${BASE_URL}/devices/`);
+}
+export const getDevicesByRoom = async (roomId) => {
+    return await axios.get(`${BASE_URL}/devices/room/${roomId}`);
+}
+export const createDevice = async (data) => {
+    return await axios.post(`${BASE_URL}/devices/`, data);
+}
+export const controlDevice = async (id, data) => {
+    return await axios.patch(`${BASE_URL}/devices/${id}/control`, data);
+}
+export const deleteDevice = async (id) => {
+    return await axios.delete(`${BASE_URL}/devices/${id}`);
+}
+export const getLogs = async () => {
+    return await axios.get(`${BASE_URL}/logs/`);
+}
+export const createSchedule = async (data) => {
+    return await axios.post(`${BASE_URL}/schedules/`, data);
+}
+export const getSchedules = async () => {
+    return await axios.get(`${BASE_URL}/schedules/`);
+}
+export const createGuest = async (data) => {
+    return await axios.post(`${BASE_URL}/guests/`, data);
+}
